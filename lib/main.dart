ab135696488fbc4b1c7fd26cc63286b7ea4053f9
ashikmhs <ashikmahmud113@gmail.com>
2020-10-13 20:04:42 +0600

    1st commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //State class
+  int _page = 0; //initialize page index
+  GlobalKey _bottomNavigationKey = GlobalKey(); // key for the bottomNavBar
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,27 +36,44 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        color: Colors.deepOrange,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'This is just a demo app',
-              style: TextStyle(color: Colors.white),
+          color: Colors.blueAccent,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  _page.toString(),
+                  textScaleFactor: 10.0,
+                ),
+                RaisedButton(
+                  child: Text('Go to page of index 2'),
+                  onPressed: () {
+                    //Page change using state does the same as clicking index 1 navigation button
+                    final CurvedNavigationBarState navBarState =
+                        _bottomNavigationKey.currentState;
+                    navBarState.setPage(2);
+                  },
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
       bottomNavigationBar: CurvedNavigationBar(
-        color: Colors.white,
-        backgroundColor: Colors.deepOrange,
-        buttonBackgroundColor: Colors.white,
-        height: 50,
+        key: _bottomNavigationKey, // puting the key that we created before
+        color: Colors.white, // this color is the main nav Bar color
+        backgroundColor: Colors
+            .blueAccent, // this color is the color that will blend with body
+        buttonBackgroundColor:
+            Colors.white, // this color is the icons background color
+        height: 50, // nav bar's height
         items: [
           Icon(
-            Icons.verified_user,
+            Icons.home,
+            size: 20,
+            color: Colors.black,
+          ),
+          Icon(
+            Icons.search,
             size: 20,
             color: Colors.black,
           ),
@@ -66,9 +87,20 @@ class _HomePageState extends State<HomePage> {
             size: 20,
             color: Colors.black,
           ),
+          Icon(
+            Icons.favorite,
+            size: 20,
+            color: Colors.black,
+          ),
         ],
+        animationDuration: Duration(milliseconds: 200), //animation duration
+        animationCurve: Curves.ease, //animation style
+        index: 2, //which index will be selected declared here
         onTap: (index) {
-          debugPrint("current index is $index");
+          //Handle button tap
+          setState(() {
+            _page = index;
+          });
         },
       ),
     );
